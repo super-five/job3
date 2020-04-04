bede20756ce369094eafdb758ea16007928a0c37
fivesuper <863689218@qq.com>
2020-04-04 23:29:26 +0800

    hahahah

--- src/main/java/com/example/common/CommonController.java
+++ src/main/java/com/example/common/CommonController.java
@@ -2,8 +2,11 @@ package com.example.common;
 
 
 public interface CommonController<T> {
+	//这里是公共的接口
 	 ResultMap<T> add(T t);
 	 ResultMap<T> delete(int t);
 	 ResultMap<T> update(T t);
 	 ResultMap<T> getList(T t);
+	 
+	 
 }
