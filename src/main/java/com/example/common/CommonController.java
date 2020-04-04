07cd59ef7f689fc5a8890c350d896cd9e8802be3
huazai <1285202070@qq.com>
2020-04-04 23:25:41 +0800

    11

--- src/main/java/com/example/common/CommonController.java
+++ src/main/java/com/example/common/CommonController.java
@@ -2,8 +2,8 @@ package com.example.common;
 
 
 public interface CommonController<T> {
-	public ResultMap<T> add(T t);
-	public ResultMap<T> delete(int t);
-	public ResultMap<T> update(T t);
-	public ResultMap<T> getList(T t);
+	 ResultMap<T> add(T t);
+	 ResultMap<T> delete(int t);
+	 ResultMap<T> update(T t);
+	 ResultMap<T> getList(T t);
 }
